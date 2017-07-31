d278ef4b850ab7012dfcf1e77fb12eafb19e86d3
Aury Rodriguez <arm010286@hotmail.com>
2017-07-30 22:51:06 -0400

    config file

---
+++ db/config.js
@@ -0,0 +1,24 @@
+const options = {
+  query: (e) => {
+    console.log(e.query);
+  }
+};
+
+const pgp = require('pg-promise')(options);
+//database conection
+//returns a pg-promise instance
+function setDatabase() {
+  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    return pgp({
+      database: 'todo_list_dev',
+      port: 5432,
+      host: 'localhost',
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+}
+
+const db = setDatabase();
+//exports the database Conection
+module.exports = db;
